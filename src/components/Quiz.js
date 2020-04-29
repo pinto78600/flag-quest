9e0d2b1081eaf1fb6ff4706a0db86e07b30f86e1
voinot <flovno@gmail.com>
2020-04-29 18:09:35 +0200

    merge de dev remove app.css and add champReponse

---
+++ src/components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChampReponse from '../ChampReponse';
+
+ 
+
+class Quiz extends React.Component{
+render(){
+  return(
+    <ChampReponse />
+  )
+}
+}
+
+export default Quiz;
