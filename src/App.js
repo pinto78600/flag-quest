0e77f6b59cdb7a9871758249c1df50ada6f981a9
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-04-29 18:38:43 +0200

    router fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {browserRouter as Router ,Route , Switch} from 'react-router-dom' 
+import {BrowserRouter as Router ,Route , Switch, Link} from 'react-router-dom' 
+
+import Quiz from './components/Quiz'
  
 
 class App extends React.Component{
 render(){
   return(
     <Router>
+      <Link to="/quiz"> Quiz</Link>
       <Switch>
-        <Route exact path="/" component={HomePage}/>
+        {/* <Route exact path="/" component={HomePage}/> */}
         <Route path="/quiz" component={Quiz}/>
-        <Route path='/score' component={Score}/>
+        {/* <Route path='/score' component={Score}/> */}
       </Switch>
     </Router>
   )
