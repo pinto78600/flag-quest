2c52d5568a6bec8cec3ad0ba96417155608d2f3e
voinot <flovno@gmail.com>
2020-04-30 12:08:28 +0200

    add style background for home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,14 +7,10 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className="Home">
-                
                 <header>
-                    
                     <h1>Flag Quest</h1>
                     <h2>Welcome to this extraordinary flag knowledge quiz !</h2>
-
                 </header>
-            
                 <div>
                     <img src={Map} alt="World map" className="Background" />
                 </div>
