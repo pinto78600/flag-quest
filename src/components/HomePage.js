6577679229245d505dc84c093b8403a62a6bdccd
voinot <flovno@gmail.com>
2020-04-30 10:50:25 +0200

    add images and home page backgound

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.css'
+import Map from '../images/map.png'
 
 class HomePage extends React.Component {
     render() {
@@ -7,7 +8,11 @@ class HomePage extends React.Component {
             <div className="Home">
                 <header>
                     <h1>Flag Quest</h1>
+                    <h2>Welcome to this extraordinary flag knowledge quiz</h2>
                 </header>
+                <div>
+                    <img src={Map} alt="" className="Background" />
+                </div>
             </div>
         )
     }
